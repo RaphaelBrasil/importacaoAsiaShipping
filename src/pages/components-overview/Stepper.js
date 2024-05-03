032af97736ea67d2037beeabd3c91a4038960193
Raphaelbrothermotos <analista3@brothermotos.com.br>
2024-05-03 11:32:17 -0300

    FirstCommit

--- src/pages/components-overview/Stepper.js
+++ src/pages/components-overview/Stepper.js
@@ -14,6 +14,7 @@ import VideoLabelIcon from '@mui/icons-material/VideoLabel';
 import { Card, Typography, CardContent } from '@mui/material';
 import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import dataSingle from './../../assets/mockdatasingle.json';
 
 // Custom Step Icon
 const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
@@ -81,6 +82,42 @@ const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   }
 }));
 
+const verificaFreeTime = (data) => {
+  // Suponha que data.chegada.free_time e data.embarque.chegada_porto sejam strings no formato 'YYYY-MM-DD'
+  const chegadaPorto = new Date(data.embarque.chegada_porto);
+  const freeTime = parseInt(data.chegada.free_time); // Converter para número inteiro
+
+  // Verificar se chegadaPorto e freeTime são válidos
+  if (!isNaN(chegadaPorto.getTime()) && !isNaN(freeTime)) {
+    // Adicionar o tempo livre à data de chegada ao porto
+    const prazo = new Date(chegadaPorto.getTime() + freeTime * 24 * 60 * 60 * 1000); // Converter dias para milissegundos
+    const dia = prazo.getDate();
+    const mes = prazo.getMonth() + 1; // Adicionando 1 porque os meses em JavaScript são baseados em zero
+    const ano = prazo.getFullYear();
+    return `${dia < 10 ? '0' + dia : dia}/${mes < 10 ? '0' + mes : mes}/${ano}`;
+  } else {
+    return 'Datas de chegada ou tempo livre inválidos.';
+  }
+};
+
+const converteData = (data) => {
+  // Suponha que data.chegada.free_time e data.embarque.chegada_porto sejam strings no formato 'YYYY-MM-DD'
+  const dataConvertida = new Date(data);
+
+  // Verificar se a data é válida
+  if (!isNaN(dataConvertida.getTime())) {
+    // Obter o dia, mês e ano da data convertida
+    const dia = dataConvertida.getDate();
+    const mes = dataConvertida.getMonth() + 1; // Adicionando 1 porque os meses em JavaScript são baseados em zero
+    const ano = dataConvertida.getFullYear();
+
+    // Formatar a data como "DD/MM/YYYY"
+    return `${dia < 10 ? '0' + dia : dia}/${mes < 10 ? '0' + mes : mes}/${ano}`;
+  } else {
+    return 'Data inválida.';
+  }
+};
+
 // Componente Personalizado do Stepper
 const stepsEmbarque = ['Etapa 1', 'Etapa 2', 'Etapa 3', 'Etapa 4'];
 const stepsChegada = ['Etapa 5', 'Etapa 6', 'Etapa 7', 'Etapa 8'];
@@ -88,6 +125,8 @@ const stepsChegada = ['Etapa 5', 'Etapa 6', 'Etapa 7', 'Etapa 8'];
 export default function CustomizedSteppers() {
   const [activeStepEmbarque, setActiveStepEmbarque] = useState(0);
   const [activeStepChegada, setActiveStepChegada] = useState(0);
+
+  const [data] = useState(dataSingle);
   // eslint-disable-next-line no-unused-vars
   const [status, setStatus] = useState();
 
@@ -127,65 +166,28 @@ export default function CustomizedSteppers() {
             <strong>Embarque</strong>
           </Typography>
           <Typography variant="body1">
-            <strong>EMPRESA:</strong> [Nome da Empresa]
+            <strong>EMPRESA:</strong> {data.embarque.empresa}
             <br />
-            <strong>AGENTE DE FRETE:</strong> [Agente de Frete]
+            <strong>AGENTE DE FRETE:</strong> {data.embarque.agente_frete}
             <br />
-            <strong>EXPORT:</strong> [Informação Export]
+            <strong>EXPORT:</strong> {data.embarque.export}
             <br />
-            <strong>ARMADOR:</strong> [Nome do Armador]
+            <strong>ARMADOR:</strong> {data.embarque.armador}
             <br />
-            <strong>CONTAINER:</strong> [Número do Container]
+            <strong>CONTAINER:</strong> {data.embarque.container}
             <br />
-            <strong>BL Nº:</strong> [Número do BL]
+            <strong>BL Nº:</strong> {data.embarque.numero_bl}
             <br />
-            <strong>BL:</strong> [BL]
+            <strong>BL:</strong> {data.embarque.bl_status ? 'OK' : ''}
             <br />
-            <strong>FATURA:</strong> [Fatura]
+            <strong>FATURA:</strong> {data.embarque.fatura}
             <br />
-            <strong>CHEGADA PORTO:</strong> [Data de Chegada no Porto]
+            <strong>CHEGADA PORTO:</strong> {converteData(data.embarque.chegada_porto)}
             <br />
           </Typography>
         </CardContent>
       </Card>
       <div style={{ display: 'flex', gap: '20px' }}>
-        <Card sx={{ display: 'flex', flexDirection: 'column', flex: 1, backgroundColor: '#bae7ff' }}>
-          <CardContent>
-            <Typography variant="h2" gutterBottom>
-              <strong>Chegada</strong>
-            </Typography>
-            <Typography variant="body1">
-              <strong>DOCS:</strong> [Documentos]
-              <br />
-              <strong>DATA DI:</strong> [Data DI]
-              <br />
-              <strong>DI Nº:</strong> [Número DI]
-              <br />
-              <strong>CE PROCESSO:</strong> [CE Processo]
-              <br />
-              <strong>STATUS:</strong> [Status]
-              <br />
-              <strong>GLME:</strong> [GLME]
-              <br />
-              <strong>NF:</strong> [Nota Fiscal]
-              <br />
-              <strong>CIPP AVERB.:</strong> [CIPP Averb]
-              <br />
-              <strong>CTE:</strong> [CTE]
-              <br />
-              <strong>CHEGADA NA EMPRESA:</strong> [Data de Chegada na Empresa]
-              <br />
-              <strong>FREE TIME:</strong> [Free Time]
-              <br />
-              <strong>FREE TIME END:</strong> [Free Time End]
-              <br />
-              <strong>TRANSP.:</strong> [Transportadora]
-              <br />
-              <strong>NF / CTE:</strong> [Nota Fiscal / CTE]
-              <br />
-            </Typography>
-          </CardContent>
-        </Card>
         <Card sx={{ flex: 1, backgroundColor: '#bae7ff' }}>
           <CardContent>
             <Typography variant="h2" gutterBottom>
@@ -205,6 +207,41 @@ export default function CustomizedSteppers() {
                 {activeStepChegada === stepsChegada.length - 1 ? 'Reset' : 'Next'}
               </Button>
             </Stack>
+          </CardContent>
+        </Card>
+        <Card sx={{ display: 'flex', flexDirection: 'column', flex: 1, backgroundColor: '#bae7ff' }}>
+          <CardContent>
+            <Typography variant="h2" gutterBottom>
+              <strong>Chegada</strong>
+            </Typography>
+            <Typography variant="body1">
+              <strong>DOCS:</strong> {data.chegada.documentacao ? 'OK' : ''}
+              <br />
+              <strong>DATA DI:</strong> {data.chegada.data_di}
+              <br />
+              <strong>DI Nº:</strong> {data.chegada.numero_di}
+              <br />
+              <strong>CE PROCESSO:</strong> {data.chegada.ce_processo}
+              <br />
+              <strong>STATUS:</strong> {data.chegada.status}
+              <br />
+              <strong>GLME:</strong> {data.chegada.glme ? 'OK' : ''}
+              <br />
+              <strong>NF_Status:</strong> {data.chegada.nf_status ? 'OK' : ''}
+              <br />
+              <strong>CIPP AVERB.:</strong> {data.chegada.cipp_averb ? 'OK' : ''}
+              <br />
+              <strong>CHEGADA NA EMPRESA:</strong> {converteData(data.chegada.chegada_empresa)}
+              <br />
+              <strong>FREE TIME:</strong> {data.chegada.free_time}
+              <br />
+              <strong>FREE TIME END:</strong> {verificaFreeTime(data)}
+              <br />
+              <strong>TRANSP.:</strong> {data.chegada.transportadora}
+              <br />
+              <strong>NF / CTE:</strong> {data.chegada.nf} / {data.chegada.cte}
+              <br />
+            </Typography>
           </CardContent>
         </Card>
       </div>
